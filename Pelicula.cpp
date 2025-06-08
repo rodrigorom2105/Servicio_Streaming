41b297a23265ea3668da15f6acd240deb62682f2
Rodrigo Romero <rocobros21@gmail.com>
2025-06-08 11:32:29 -0600

    Archivos de clases

---
+++ Pelicula.cpp
@@ -0,0 +1,13 @@
+#include "Pelicula.h"
+#include <iostream>
+using namespace std;
+
+Pelicula::Pelicula(string id, string titulo, string genero, int duracion)
+		: Video(id, titulo, genero, duracion) {}
+
+void Pelicula::mostrarInformacion() const
+{
+	cout << "Pelicula: " << titulo << " (" << genero << ", " << duracion << " min)";
+	double cal = obtenerCalificacionPromedio();
+	cout << " | Calificación: " << (cal == -1 ? "SC" : to_string(cal)) << endl;
+}
---
+++ Video.cpp
@@ -0,0 +1,36 @@
+#include "Video.h"
+#include <numeric>
+#include <iostream>
+using namespace std;
+
+Video::Video(string id, string titulo, string genero, int duracion)
+{
+	Video::id = id;
+	Video::titulo = titulo;
+	Video::genero = genero;
+	Video::duracion = duracion;
+}
+
+void Video::agregarCalificacion(int calificacion)
+{
+	if (calificacion >= 1 && calificacion <= 5)
+	{
+		calificaciones.push_back(calificacion);
+	}
+	else
+	{
+		cerr << "Calificación inválida. Debe estar entre 1 y 5." << endl;
+	}
+}
+
+double Video::obtenerCalificacionPromedio() const
+{
+	if (calificaciones.empty())
+		return -1; // No hay calificaciones, retorna -1 para indicar "Sin Calificación"
+	double suma = accumulate(calificaciones.begin(), calificaciones.end(), 0);
+	return suma / calificaciones.size();
+}
+
+string Video::getTitulo() const { return titulo; }
+
+string Video::getGenero() const { return genero; }
